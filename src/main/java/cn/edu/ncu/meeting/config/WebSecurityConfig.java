b0e955d395cad828acd96193a57b25508a7a52e3
LinLorry <lin864464995@163.com>
2019-10-12 11:35:40 +0800

    Add Web Security Config.

---
+++ src/main/java/cn/edu/ncu/meeting/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package cn.edu.ncu.meeting.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthenticationFilter authenticationFilter;
+
+    private final TokenAuthenticationEntryPoint authenticationEntryPoint;
+
+    public WebSecurityConfig(AuthenticationFilter authenticationFilter, TokenAuthenticationEntryPoint authenticationEntryPoint) {
+        this.authenticationFilter = authenticationFilter;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests().antMatchers("/api/user/token").permitAll().and()
+            .authorizeRequests().antMatchers("/api/user/registry").permitAll()
+            .anyRequest().authenticated().and()
+            .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
