9c78c7934934470ad7bd0dd6b7925e0679ddbf10
LinLorry <lin864464995@163.com>
2019-10-16 18:31:57 +0800

    Update User Repository annotation.

--- src/main/java/cn/edu/ncu/meeting/user/repo/UserRepository.java
+++ src/main/java/cn/edu/ncu/meeting/user/repo/UserRepository.java
@@ -4,6 +4,12 @@ import cn.edu.ncu.meeting.user.model.User;
 import cn.edu.ncu.meeting.user.model.UserKey;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * User Repository
+ * @author lorry
+ * @author lin864464995@163.com
+ * @see org.springframework.data.repository.CrudRepository
+ */
 public interface UserRepository extends CrudRepository<User, UserKey> {
     User findById(Integer id);
 
