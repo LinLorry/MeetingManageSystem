5ef5b9c3ecf6933411b0e8756099a1c8be6caf44
LinLorry <lin864464995@163.com>
2019-10-16 19:51:48 +0800

    Fix User key.

--- src/main/java/cn/edu/ncu/meeting/user/repo/UserRepository.java
+++ src/main/java/cn/edu/ncu/meeting/user/repo/UserRepository.java
@@ -1,7 +1,6 @@
 package cn.edu.ncu.meeting.user.repo;
 
 import cn.edu.ncu.meeting.user.model.User;
-import cn.edu.ncu.meeting.user.model.UserKey;
 import org.springframework.data.repository.CrudRepository;
 
 /**
