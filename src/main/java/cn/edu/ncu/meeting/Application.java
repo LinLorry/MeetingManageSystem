bdf12990275f233259e65c0515f889563098f961
LinLorry <lin864464995@163.com>
2019-10-16 18:31:42 +0800

    Update Application annotation.

--- src/main/java/cn/edu/ncu/meeting/Application.java
+++ src/main/java/cn/edu/ncu/meeting/Application.java
@@ -3,6 +3,11 @@ package cn.edu.ncu.meeting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Application.
+ * @author lorry
+ * @author lin864464995@163.com
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
