5f49700637a5215c3146867f92ae05a68f24135b
LinLorry <lin864464995@163.com>
2019-10-17 15:50:40 +0800

    Add Method Security.

--- src/main/java/cn/edu/ncu/meeting/config/WebSecurityConfig.java
+++ src/main/java/cn/edu/ncu/meeting/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import cn.edu.ncu.meeting.security.TokenAuthenticationEntryPoint;
 import cn.edu.ncu.meeting.user.UserService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final AuthenticationFilter authenticationFilter;
 
