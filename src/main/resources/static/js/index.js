18c32e3b738d22323f8682377c845a35c75ab5c1
LinLorry <lin864464995@163.com>
2019-10-21 17:44:27 +0800

    add load Menus.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,44 +1,41 @@
-var stronge = window.localStorage;
+let stronge = window.localStorage;
 
-var url = "/api/user/menus";
+let url = "/api/user/menus";
+
+if (stronge.menus == null || stronge.menus == "") {
+    loadMenus();
+}
 
 window.onload = function() {
-    var select_ul = document.getElementById("select-ul");
-    var login_a = this.document.getElementById("login_a");
+    let select_ul = document.getElementById("select-ul");
+    let menus = this.JSON.parse(stronge.menus);
 
-    var profile_li = this.document.createElement("li");
-    var profile_a = this.document.createElement("a");
+    if (menus.length != 0) {
+        select_ul.innerHTML = "";
+        menus.forEach(element => {
+            let li = this.document.createElement("li");
+            let a = this.document.createElement("a");
+            li.appendChild(a);
+    
+            a.innerHTML = element.name;
+            a.href = element.url;
+    
+            select_ul.appendChild(li);
+        });
+    }
+}
 
-    profile_li.id = "profile_li";
-    profile_a.id = "profile_a";
-    profile_a.innerHTML = "个人信息";
-    profile_li.appendChild(profile_a);
+function loadMenus() {
+    let url = "/api/user/menus";
 
     fetch(url, {
         headers: {
-            "Authorization": "Meeting " + stronge.token
+            "Authorization": "Meeting " + window.localStorage.token
         },
         method: "GET"
     })
     .then(response => response.json())
     .then(function(json) {
-        if (json.status == 1) {
-            var data = json.data;
-
-            select_ul.innerHTML = "";
-
-            for (let index = 0; index < data.length; index++) {
-                const element = data[index];
-                let li = this.document.createElement("li");
-                var a = this.document.createElement("a");
-                li.appendChild(a);
-
-                a.innerHTML = element.name;
-                a.href = element.url;
-
-                select_ul.appendChild(li); 
-            }
-        }
-    })
+        window.localStorage.menus = JSON.stringify(json.data);
+    });
 }
-
