3a4453b8d57c4e4b60885cd0649298dc68884140
LinLorry <lin864464995@163.com>
2019-10-21 15:02:43 +0800

    Update  login .

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,7 +1,29 @@
 function login() {
     var username = loginBox.username.value;
     var password = loginBox.password.value;
+    var data = {
+        username: username,
+        password: password
+    };
 
-    console.log(username);
-    console.log(password);
+    url = "/api/user/token";
+
+    fetch(url, {
+        body: JSON.stringify(data),
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: "POST"
+    })
+    .then(response => response.json())
+    .then(function(data) {
+        if (data.status == 1) {
+            window.localStorage.token = data.token;
+            window.location.href = "/index.html";
+        } else {
+            var hint = document.getElementById("hint");
+            hint.style.display = "block"; 
+            hint.innerHTML = data.message;
+        } 
+    });
 }
