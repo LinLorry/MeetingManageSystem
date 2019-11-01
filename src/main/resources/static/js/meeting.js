878af6bec33620d486a10805772c29e125bef2ae
LinLorry <lin864464995@163.com>
2019-11-01 15:54:35 +0800

    Add meeting

---
+++ src/main/resources/static/js/meeting.js
@@ -0,0 +1,153 @@
+getMenus();
+
+var id = getId();
+
+window.onload = function() {
+    loadMenus();
+
+    if (id === this.undefined) return;
+
+    let url = new URL("/api/meeting/get", "http://" + document.domain);
+
+    url.searchParams.append("id", id);
+    let token = this.localStorage.token;
+    let headers;
+
+    if (token != null && token != "") {
+        headers = {
+            "Authorization": "Meeting " + token
+        };
+    }
+
+    this.fetch(url, {
+        method: "GET",
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(function(json) {
+        if (json.status == 1) {
+            let data = json.data[0];
+
+            let name = document.getElementById("name");
+            let hostUser = document.getElementById("host-user");
+            let time = document.getElementById("time");
+            let location = document.getElementById("location");
+            let star = document.getElementById("star");
+            let comment = document.getElementById("comment");
+            let needList = document.getElementById("need-list");
+
+            name.innerHTML = data.name;
+            hostUser.innerHTML = data.holdUser.name;
+            time.innerHTML = getDateString(data.time)
+            location.innerHTML = data.location;
+            star.innerHTML = data.star;
+            comment.innerHTML = data.comment;
+
+            if (data.needGender) {
+                let li = document.createElement("li");
+                li.innerHTML = "性别";
+                needList.appendChild(li);
+            }
+
+            if (data.needIdCard) {
+                let li = document.createElement("li");
+                li.innerHTML = "身份证号";
+                needList.appendChild(li);
+            }
+
+            if (data.needName) {
+                let li = document.createElement("li");
+                li.innerHTML = "姓名";
+                needList.appendChild(li);
+            }
+
+            if (data.needOrganization) {
+                let li = document.createElement("li");
+                li.innerHTML = "工作单位";
+                needList.appendChild(li);
+            }
+
+                if (needList.children.length == 0) {
+                document.getElementById("need-list-li").style.display = "none";
+            }
+
+            if (data.needParticipateTime) {
+                document.getElementById("ParticipateTime-label").style.display = "block";
+            }
+
+        } else {
+            let commonBox = document.getElementById("comment-box");
+            let message = document.createElement("p");
+
+            message.innerHTML = json.message;
+            message.style.fontSize = "1.4em";
+            commonBox.innerHTML = ""
+            commonBox.appendChild(message);
+        }
+    })
+}
+
+function join() {
+    let url = "/api/meeting/join";
+    let participateTime = joinBox.participateTime.value;
+    let needHotel = joinBox.needHotel.checked;
+    let data = {
+        meetingId: id,
+        participateTime: participateTime,
+        needHotel: needHotel
+    };
+    let token = this.localStorage.token;
+
+    if (token == null || token == "" || token === undefined) {
+        window.alert("报名会议需先登陆！")
+        location.href = "/login.html"
+        return;
+    }
+
+    let headers = {
+        'Authorization': "Meeting " + token,
+        'content-type': 'application/json'
+    };
+
+    fetch(url, {
+        body: JSON.stringify(data),
+        method: "POST",
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(function(json) {
+        var hint = document.getElementById("hint");
+        hint.style.display = "block"; 
+        hint.innerHTML = json.message;
+    })
+
+}
+
+function getId() {
+    let search = this.location.search;
+
+    if (search.indexOf("?") == -1) {
+        this.location.href = "/index.html";
+        return undefined;
+    }
+    
+    search = search.substr(1);
+    let result = search.split("=")
+    
+    if (result[0] != "id") {
+        this.location.href = "/index.html";
+        return undefined;
+    }
+
+    return result[1];
+}
+
+function getDateString(time) {
+    let date = new Date(time);
+
+    return date.getFullYear() + '年' 
+    + date.getMonth() + '月' 
+    + date.getDay() + '日' 
+    + date.getHours() + '点' 
+    + date.getMinutes() + '分';
+}
