73e55cf53af7d2641735dc1cac656acb5adfacf9
LinLorry <lin864464995@163.com>
2019-11-01 20:16:49 +0800

    Add get Hot meeting function.

--- src/main/java/cn/edu/ncu/meeting/meeting/repo/MeetingRepository.java
+++ src/main/java/cn/edu/ncu/meeting/meeting/repo/MeetingRepository.java
@@ -1,7 +1,10 @@
 package cn.edu.ncu.meeting.meeting.repo;
 
 import cn.edu.ncu.meeting.meeting.model.Meeting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -12,4 +15,16 @@ import org.springframework.data.repository.CrudRepository;
  * @see org.springframework.data.jpa.repository.JpaSpecificationExecutor
  */
 public interface MeetingRepository extends CrudRepository<Meeting, Long>, JpaSpecificationExecutor<Meeting> {
+    /**
+     * Get the most join user meeting which not over time.
+     * @param pageable the page number
+     * @return Meeting Page.
+     */
+    @Query(value = "SELECT m " +
+            "FROM Meeting m " +
+            "LEFT JOIN MeetingJoinUser mju on mju.meeting.id = m.id " +
+            "where m.time > current_time " +
+            "group by m.id " +
+            "order by count(mju) desc")
+    Page<Meeting> findAllHot(Pageable pageable);
 }
