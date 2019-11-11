f6a8d569a9418cbe5eddad49017872e63c817a52
LinLorry <lin864464995@163.com>
2019-11-11 17:10:32 +0800

    Update loadMenus into utils.

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,5 +1,59 @@
 const meetingFields = ["location", "star", "hotel", "comment"];
 
+function getProfile() {
+    let url = "/api/user/profile";
+    let headers = {
+        "Authorization": "Meeting " + localStorage.token
+    };
+
+    fetch(url, {
+        method: "GET",
+        headers: headers
+    })
+    .then(response => response.json())
+    .then(function(json) {
+        if (json.status === 1) {
+            window.localStorage.profile = JSON.stringify(json.data);
+        }
+    })
+}
+
+function getMenus() {
+    let url = "/api/user/menus";
+
+    fetch(url, {
+        headers: {
+            "Authorization": "Meeting " + window.localStorage.token
+        },
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(function(json) {
+        if (json.status == 1) {
+            window.localStorage.menus = JSON.stringify(json.data);
+        }
+    });
+}
+
+function loadMenus() {
+    let select_ul = document.getElementById("select-ul");
+    let menus = this.JSON.parse(window.localStorage.menus);
+
+    if (menus.length != 0) {
+        select_ul.innerHTML = "";
+        menus.forEach(element => {
+            let li = this.document.createElement("li");
+            let a = this.document.createElement("a");
+            li.appendChild(a);
+    
+            a.innerHTML = element.name;
+            a.href = element.url;
+    
+            select_ul.appendChild(li);
+        });
+    }
+}
+
 function judgeLogin() {
     let token = localStorage.token;
     let userProfileURL = "/api/user/profile";
@@ -19,6 +73,11 @@ function judgeLogin() {
     } else {
         window.location.href = "/index.html";
     }
+}
+
+function getUserId() {
+    let profile = this.JSON.parse(window.localStorage.profile);
+    return profile.id;
 }
 
 function getDateString(time) {
