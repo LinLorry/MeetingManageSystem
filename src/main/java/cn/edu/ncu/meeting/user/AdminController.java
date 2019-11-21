60e633751e9a35cd5f3f85fdb97ae2a8b742eb05
LinLorry <lin864464995@163.com>
2019-11-21 20:44:20 +0800

    Update admin controller.

--- src/main/java/cn/edu/ncu/meeting/user/AdminController.java
+++ src/main/java/cn/edu/ncu/meeting/user/AdminController.java
@@ -1,10 +1,13 @@
 package cn.edu.ncu.meeting.user;
 
 import cn.edu.ncu.meeting.meeting.MeetingService;
+import cn.edu.ncu.meeting.meeting.model.Meeting;
 import cn.edu.ncu.meeting.user.model.User;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 /**
  * Admin Controller
@@ -34,7 +37,7 @@ public class AdminController {
             response.put("data", user);
         } catch (UsernameNotFoundException e) {
             response.put("status", 0);
-            response.put("message", "This user not exist");
+            response.put("message", "This user not exist.");
         }
 
         return response;
@@ -53,24 +56,52 @@ public class AdminController {
         return response;
     }
 
-
-    // TODO Delete Meeting Api.
+    /**
+     * Delete Meeting Api
+     * @param id the meeting id.
+     * @return {
+     *     "status": 1,
+     *     "message": "Delete meeting success."
+     * }
+     */
     @ResponseBody
     @DeleteMapping("/meeting")
     public JSONObject deleteMeeting(@RequestParam long id) {
         JSONObject response = new JSONObject();
 
+        meetingService.removeMeetingById(id);
+        response.put("status", 1);
+        response.put("message", "Delete meeting success.");
+
         return response;
     }
 
-    // TODO Get Join Meeting User Info Api.
+    /**
+     * Get Meeting Join User Api.
+     * @param id the meeting id.
+     * @return if meeting exist return {
+     *     "status": 1,
+     *     "message": "Get meeting join user success."
+     * } else if meeting not exist return {
+     *     "status": 0,
+     *     "message": "This meeting not exist."
+     * }
+     */
     @ResponseBody
     @GetMapping("/join")
     public JSONObject getMeetingJoinUser(@RequestParam long id) {
         JSONObject response = new JSONObject();
 
+        try {
+            Meeting meeting = meetingService.loadMeetingById(id);
+            response.put("status", 1);
+            response.put("message", "Get meeting join user success.");
+            response.put("data", meeting.getJoinUserSet());
+        } catch (NoSuchElementException e) {
+            response.put("status", 0);
+            response.put("message", "This meeting not exist.");
+        }
 
         return response;
     }
-
 }
